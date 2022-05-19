6f6936ae27e023687cba0df07772afda355fcb85
dankcuddlybear <alexandermasokin@gmail.com>
2022-05-19 11:09:31 +0100

    Still working on it...

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-for dir in */ ; do
-    cd $dir
-    echo "Building $dir"
-    makepkg || exit 1
-    cd ../
+[ $(whoami) == "root" ] && echo "[ERROR] You must run this script as a non-root user with sudo priviliges." && exit 1
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+shopt -s extglob
+for dir in $SCRIPT_DIR/!(.git|__PKG|__PROTO)/   
+	cd $dir
+	echo "Building $dir"
+	makepkg -sr || exit 1
+	mv */pkg.tar.zst ../__PKG
+	rm -rf pkg src
+	cd ../
 done
+__PKG/repo-update.sh

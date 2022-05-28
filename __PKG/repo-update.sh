c1c208d084cc1edbc041ab241a92599fdaca26b7
dankcuddlybear <alexandermasokin@gmail.com>
2022-05-28 14:44:41 +0100

    Fixed repo name error

--- __PKG/repo-update.sh
+++ __PKG/repo-update.sh
@@ -2,7 +2,7 @@
 REPO_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_NAME="vapourepo"
 rm $REPO_DIR/$REPO_NAME.db $REPO_DIR/$REPO_NAME.db.tar.gz $REPO_DIR/$REPO_NAME.files $REPO_DIR/$REPO_NAME.files.tar.gz
-repo-add $REPO_DIR/vapour-os-repo.db.tar.gz $REPO_DIR/*.pkg.tar.zst
+repo-add $REPO_DIR/$REPO_NAME.db.tar.gz $REPO_DIR/*.pkg.tar.zst
 rm $REPO_DIR/$REPO_NAME.db $REPO_DIR/$REPO_NAME.files
 mv $REPO_DIR/$REPO_NAME.db.tar.gz $REPO_DIR/$REPO_NAME.db
 mv $REPO_DIR/$REPO_NAME.files.tar.gz $REPO_DIR/$REPO_NAME.files

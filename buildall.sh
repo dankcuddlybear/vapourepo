235189c10e1a33636954e1ab2de98561c4f286f8
dankcuddlybear <alexandermasokin@gmail.com>
2022-06-05 17:25:51 +0100

    Updated ALL pkgbuilds with my name and developer email

--- buildall.sh
+++ buildall.sh
@@ -5,10 +5,11 @@ shopt -s extglob
 for dir in $SCRIPT_DIR/!(.git|liveusb-installer|__PKG|__PROTO)/; do
 	cd $dir
 	echo "Building $dir"
-	makepkg -cdf || exit 1
+	makepkg -cdf
 	mv *.pkg.tar.zst ../__PKG
 	cd ../
-	[ $dir == "hackbgrt-bin" ] && rm *.zip
-	[ $dir == "intel-opencl-runtime" ] && rm *.tgz
+	[ $dir == "hackbgrt-bin" ] && rm HackBGRT-1.5.1.zip
+	[ $dir == "intel-opencl-runtime" ] && rm l_opencl_p_18.1.0.015.tgz
+	[ $dir == "vapour-os-bootscreen" ] && rm HackBGRT-1.5.1.zip
 done
 $SCRIPT_DIR/__PKG/repo-update.sh

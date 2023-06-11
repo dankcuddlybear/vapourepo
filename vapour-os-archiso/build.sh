ef55aa327138ebc8341234e98e89380b2bd74358
dankcuddlybear <38557087+dankcuddlybear@users.noreply.github.com>
2023-06-11 22:43:25 +0100

    Add files via upload

---
+++ vapour-os-archiso/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DISTRO_ID="vapour-os"
+TEMP_DIR="/tmp/$DISTRO_ID-isobuild-tmp"
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ARCHISO_PROFILE="$SCRIPT_DIR/$DISTRO_ID-live-archiso"
+SCRIPT_USER="$(whoami)"
+Error() {
+	echo "[ERROR] $1" && exit 1
+}
+[ "$SCRIPT_USER" == "root" ] && Error "Do not run this script as root."
+cp /usr/bin/cat "$ARCHISO_PROFILE/airootfs/usr/local/sbin/cat"
+sudo rm -rf "$TEMP_DIR" "$SCRIPT_DIR/out" &> /dev/null; sudo mkdir "$TEMP_DIR"
+sudo mkarchiso -v -w "$TEMP_DIR" "$ARCHISO_PROFILE" &&
+sudo chown -R 1000:1000 "$SCRIPT_DIR/out"
+read -p "Done. Press ENTER to exit."

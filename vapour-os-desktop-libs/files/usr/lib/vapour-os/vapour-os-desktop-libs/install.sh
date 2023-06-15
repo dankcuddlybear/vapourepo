01222b24796b8a16cf907a1198e17b1d67790f31
Dankcuddlybear <dankcuddlybear@proton.me>
2023-06-15 14:15:56 +0100

    Lots of fixes and improvements.

--- vapour-os-desktop-libs/files/usr/lib/vapour-os/vapour-os-desktop-libs/install.sh
+++ vapour-os-desktop-libs/files/usr/lib/vapour-os/vapour-os-desktop-libs/install.sh
@@ -3,4 +3,4 @@ touch /etc/vconsole.conf
 unset FONT
 . /etc/vconsole.conf
 [ -z "$FONT" ] && echo "FONT=ter-116b" >> /etc/vconsole.conf
-systemctl --now enable bluetooth ModemManager ipp-usb cups.socket cups-browsed
+systemctl --now enable ananicy-cpp bluetooth ModemManager ipp-usb cups.socket cups-browsed

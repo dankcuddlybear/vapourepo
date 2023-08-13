d0a6c5a18503927f6d6f6cba9b43e73d73c575df
Dankcuddlybear <dankcuddlybear@proton.me>
2023-08-13 01:19:40 +0100

    kgvjbh

--- vapour-os-liveusb-installer/files/usr/lib/vapour-os/chroot-setup.sh
+++ vapour-os-liveusb-installer/files/usr/lib/vapour-os/chroot-setup.sh
@@ -3,7 +3,6 @@ DISTRO_ID="vapour-os"
 DISTRO_NAME="Vapour OS"
 [ $(whoami) != "root" ] && echo "[ERROR] You must run this script with root priviliges." && exit 1
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ARCH="$(uname -m)"
 
 # Clean up packages
 pacman --asdeps -D lib32-pipewire-jack pipewire-alsa pipewire-jack pipewire-pulse xdg-desktop-portal-gnome xdg-desktop-portal-kde xdg-desktop-portal-gtk wireplumber

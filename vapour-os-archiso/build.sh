7f900f369470f877d7351e00878ea2577f8a004e
Dankcuddlybear <dankcuddlybear@proton.me>
2023-08-13 01:03:50 +0100

    gtfvjhb

--- vapour-os-archiso/build.sh
+++ vapour-os-archiso/build.sh
@@ -3,6 +3,7 @@ DISTRO_ID="vapour-os"
 TEMP_DIR="/tmp/$DISTRO_ID-isobuild-tmp"
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ARCHISO_PROFILE="$SCRIPT_DIR/$DISTRO_ID-live-archiso"
+OUT_DIR="$SCRIPT_DIR/out"
 SCRIPT_USER="$(whoami)"
 Error() {
 	echo "[ERROR] $1" && exit 1
@@ -10,7 +11,7 @@ Error() {
 [ "$SCRIPT_USER" == "root" ] && Error "Do not run this script as root."
 cp /usr/bin/cat "$ARCHISO_PROFILE/airootfs/usr/local/sbin/"
 cp /usr/bin/vercmp "$ARCHISO_PROFILE/airootfs/usr/local/sbin/"
-sudo rm -rf "$TEMP_DIR" "$SCRIPT_DIR/out" &> /dev/null; sudo mkdir "$TEMP_DIR"
-sudo mkarchiso -v -w "$TEMP_DIR" "$ARCHISO_PROFILE" &&
-sudo chown -R 1000:1000 "$SCRIPT_DIR/out"
+sudo rm -rf "$TEMP_DIR" "$OUT_DIR" &> /dev/null; sudo mkdir "$TEMP_DIR" "$OUT_DIR"
+sudo mkarchiso -v -w "$TEMP_DIR" "$ARCHISO_PROFILE" -o "$OUT_DIR"
+sudo chown -R 1000:1000 "$OUT_DIR"
 read -p "Done. Press ENTER to exit."

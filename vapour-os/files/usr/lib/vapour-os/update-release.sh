87a45de22a20978c2da89da4665377e9f0c6aa7d
dankcuddlybear <38557087+dankcuddlybear@users.noreply.github.com>
2023-08-30 15:31:23 +0300

    Add files via upload

---
+++ vapour-os/files/usr/lib/vapour-os/update-release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DISTRO_ID="vapour-os"; DISTRO_NAME="Vapour OS"
+if [ -f "/$DISTRO_ID-live" ]; then # ArchISO build mode
+	cp /etc/os-release /usr/lib/
+	cp /usr/share/$DISTRO_ID/custom-configs/lsb-release-live /etc/lsb-release
+else # System install mode
+	cp /usr/share/$DISTRO_ID/custom-configs/os-release /usr/lib/
+	[ -f /etc/lsb-release ] && cp /usr/share/$DISTRO_ID/custom-configs/lsb-release /etc/
+fi

8d035075e6431b5c5f8de514c68f57ab69eccea9
Dankcuddlybear <dankcuddlybear@proton.me>
2023-10-28 13:00:04 +0100

    ghj

--- vapour-os-archiso/build.sh
+++ vapour-os-archiso/build.sh
@@ -12,7 +12,7 @@ Error() {
 cp /usr/bin/cat "$ARCHISO_PROFILE/airootfs/usr/local/sbin/"
 cp /usr/bin/vercmp "$ARCHISO_PROFILE/airootfs/usr/local/sbin/"
 sudo rm -rf "$TEMP_DIR" "$OUT_DIR" &> /dev/null; sudo mkdir "$TEMP_DIR" "$OUT_DIR"
-sudo pacman-key --refresh-keys
+#sudo pacman-key --refresh-keys
 sudo mkarchiso -v -w "$TEMP_DIR" "$ARCHISO_PROFILE" -o "$OUT_DIR"
 sudo chown -R $SCRIPT_USER:$SCRIPT_USER "$OUT_DIR"
 read -p "Done. Press ENTER to exit."

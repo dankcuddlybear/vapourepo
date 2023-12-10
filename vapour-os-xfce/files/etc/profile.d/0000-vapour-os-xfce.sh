a115c233dc0971c9d52ff451952b849a9c4acbfa
Dankcuddlybear <dankcuddlybear@proton.me>
2023-12-10 00:00:17 +0000

    nk

--- vapour-os-xfce/files/etc/profile.d/0000-vapour-os-xfce.sh
+++ vapour-os-xfce/files/etc/profile.d/0000-vapour-os-xfce.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 DISTRO_ID="vapour-os"
-cp -nr /usr/share/$DISTRO_ID/custom-configs/$DISTRO_ID-xfce/.config /home/$(whoami)/
+SCRIPT_USER="$(whoami)"
+if [ ! -f "/home/$SCRIPT_USER/.config/xfce4/vapour-os-xfce" ]; then
+	cp -nr "/usr/share/$DISTRO_ID/custom-configs/$DISTRO_ID-xfce/.config" "/home/$SCRIPT_USER/"
+fi

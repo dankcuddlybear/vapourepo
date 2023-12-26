2970d52ba13c81c6679ddd8159f7049ba3b7e994
Dankcuddlybear <dankcuddlybear@proton.me>
2023-12-26 11:45:40 +0000

    Bugfixes

--- set-default-permissions.sh
+++ set-default-permissions.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SetPermissions() {
-	echo "755: $1"
 	chmod 755 "$1"
 	for FILE in $(ls -A); do
 		if [ -f "$FILE" ]; then
-			echo "644: $(pwd)/$FILE"
 			chmod 644 "$(pwd)/$FILE"
 		elif [ -d "$FILE" ]; then
 			cd "$FILE"
--- vapour-os-kde/files/etc/profile.d/0000-vapour-os-kde.sh
+++ vapour-os-kde/files/etc/profile.d/0000-vapour-os-kde.sh
@@ -3,7 +3,6 @@ DISTRO_ID="vapour-os"
 SCRIPT_USER="$(whoami)"
 if [ ! -f "/home/$SCRIPT_USER/.config/KDE/vapour-os-kde" ]; then
 	cp -r "/usr/share/$DISTRO_ID/custom-configs/$DISTRO_ID-kde/.config" "/home/$SCRIPT_USER/" &> /dev/null
-else cp -nr "/usr/share/$DISTRO_ID/custom-configs/$DISTRO_ID-kde/.config" "/home/$SCRIPT_USER/" &> /dev/null; fi
-if [ ! -f "/home/$SCRIPT_USER/.local/vapour-os-kde" ]; then
+fi; if [ ! -f "/home/$SCRIPT_USER/.local/share/vapour-os-kde" ]; then
 	cp -r "/usr/share/$DISTRO_ID/custom-configs/$DISTRO_ID-kde/.local" "/home/$SCRIPT_USER/" &> /dev/null
-else cp -nr "/usr/share/$DISTRO_ID/custom-configs/$DISTRO_ID-kde/.local" "/home/$SCRIPT_USER/" &> /dev/null; fi
+fi

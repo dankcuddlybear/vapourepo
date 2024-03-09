bd3b6ea5cca82bcf542056a53a0e7788a6b079a3
dankcuddlybear <dankcuddlybear@proton.me>
2024-03-09 18:57:06 +0000

    k

--- pkgbuild/vapour-os-liveusb-installer/files/usr/lib/vapour-os/chroot-setup.sh
+++ pkgbuild/vapour-os-liveusb-installer/files/usr/lib/vapour-os/chroot-setup.sh
@@ -13,30 +13,38 @@ pacman --noconfirm -Sc
 
 # Add user
 CONTINUE=0; while [ $CONTINUE == 0 ]; do
-	echo "[ATTENTION] Please enter an owner's username. This user will have administrator priviliges."
-	unset USER_NAME; read USER_NAME
-	if [ -z "$USER_NAME" ]; then echo "[ERROR] No username provided"
-	else
-		useradd -m "$USER_NAME" && CONTINUE=1 || CONTINUE=0
-	fi
+	/usr/lib/voscfg/adduser
+	[ ! -z "$(ls -I .home-tmp /home)" ] && CONTINUE=1
 done
-passwd -d "$USER_NAME"; gpasswd -a "$USER_NAME" wheel
-CONTINUE=0; while [ $CONTINUE == 0 ]; do
-	echo "[ATTENTION] Please enter a password for that owner's account."
-	passwd "$USER_NAME" && CONTINUE=1 || CONTINUE=0
-done
+#CONTINUE=0; while [ $CONTINUE == 0 ]; do
+#	echo "[ATTENTION] Please enter an owner's username. This user will have administrator priviliges."
+#	unset USER_NAME; read USER_NAME
+#	if [ -z "$USER_NAME" ]; then echo "[ERROR] No username provided"
+#	else
+#		useradd -m "$USER_NAME" && CONTINUE=1 || CONTINUE=0
+#	fi
+#done
+#passwd -d "$USER_NAME"; gpasswd -a "$USER_NAME" wheel
+#CONTINUE=0; while [ $CONTINUE == 0 ]; do
+#	echo "[ATTENTION] Please enter a password for that owner's account."
+#	passwd "$USER_NAME" && CONTINUE=1 || CONTINUE=0
+#done
 
 # Set hostname
 CONTINUE=0; while [ $CONTINUE == 0 ]; do
-	echo "[ATTENTION] Please enter a hostname for this computer"
-	unset HOST_NAME; read HOST_NAME
-	if [ -z "$HOST_NAME" ]; then echo "[WARNING] No hostname provided"
-	else
-		echo "$HOST_NAME" > /etc/hostname
-		echo "127.0.0.1 localhost" > /etc/hosts
-		echo "::1 localhost" >> /etc/hosts
-		echo "127.0.1.1 $HOST_NAME" >> /etc/hosts
-		CONTINUE=1
-	fi
+	/usr/lib/voscfg/statichostname
+	[ ! -z "$(cat /etc/hostname)" ] && CONTINUE=1
 done
+#CONTINUE=0; while [ $CONTINUE == 0 ]; do
+#	echo "[ATTENTION] Please enter a hostname for this computer"
+#	unset HOST_NAME; read HOST_NAME
+#	if [ -z "$HOST_NAME" ]; then echo "[WARNING] No hostname provided"
+#	else
+#		echo "$HOST_NAME" > /etc/hostname
+#		echo "127.0.0.1 localhost" > /etc/hosts
+#		echo "::1 localhost" >> /etc/hosts
+#		echo "127.0.1.1 $HOST_NAME" >> /etc/hosts
+#		CONTINUE=1
+#	fi
+#done
 sync

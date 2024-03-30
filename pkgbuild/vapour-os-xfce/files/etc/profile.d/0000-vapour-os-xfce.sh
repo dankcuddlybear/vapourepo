1506f0454c5fd4ac0967abf18a5e270fa92648a5
Dankcuddlybear <dankcuddlybear@proton.me>
2024-03-30 19:36:09 +0000

    x

--- pkgbuild/vapour-os-xfce/files/etc/profile.d/0000-vapour-os-xfce.sh
+++ pkgbuild/vapour-os-xfce/files/etc/profile.d/0000-vapour-os-xfce.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-DISTRO_ID="vapour-os"
-SCRIPT_USER="$(whoami)"
-if [ ! -f "/home/$SCRIPT_USER/.config/xfce4/vapour-os-xfce" ]; then
-	cp -r "/usr/share/$DISTRO_ID/custom-configs/$DISTRO_ID-xfce/.config" "/home/$SCRIPT_USER/" &> /dev/null
-else cp -nr "/usr/share/$DISTRO_ID/custom-configs/$DISTRO_ID-xfce/.config" "/home/$SCRIPT_USER/" &> /dev/null; fi
+DISTRO_ID="vapour-os"; SCRIPT_USER="$(whoami)"
+if ([ ! -z "$DESKTOP_SESSION" ] && [ "$DESKTOP_SESSION" == "xfce" ]) || \
+([ ! -z "$XDG_DESKTOP_SESSION" ] && [ "$XDG_DESKTOP_SESSION" == "xfce" ]); then
+	export QT_QPA_PLATFORMTHEME=qt5ct
+	if [ ! -f "/home/$SCRIPT_USER/.config/xfce4/vapour-os-xfce" ]; then
+		cp -r "/usr/share/$DISTRO_ID/custom-configs/$DISTRO_ID-xfce/.config" "/home/$SCRIPT_USER/" &> /dev/null
+	else
+		cp -nr "/usr/share/$DISTRO_ID/custom-configs/$DISTRO_ID-xfce/.config" "/home/$SCRIPT_USER/" &> /dev/null
+	fi
+fi
